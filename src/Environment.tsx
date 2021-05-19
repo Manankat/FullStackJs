8098b2e15cd0201be9a7c99bb011939ab87ea3fe
Alexandre Chetafi <alexandre.chetafi@epitech.eu>
2021-05-19 14:02:17 +0200

    [ADD] adding rxjs and axios [ADD] adding userModel and tokenMode [ADD] creating authentication service

---
+++ src/Environment.tsx
@@ -0,0 +1,5 @@
+class EnvironmentProduction {
+    apiUrl = "http://localhost:4000/";
+}
+
+export const Environment = new EnvironmentProduction();
---
+++ src/Model/User.tsx
@@ -0,0 +1,11 @@
+export default class UserModel {
+    username: string;
+    email: string;
+    password: string;
+
+    constructor() {
+        this.email = '';
+        this.username = '';
+        this.password = '';
+    }
+}
