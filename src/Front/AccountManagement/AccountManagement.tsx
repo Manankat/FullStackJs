b412698eb9059c6ad9419bb9282c57e1507fbd76
Alexandre Chetafi <alexandre.chetafi@epitech.eu>
2021-05-29 21:51:40 +0200

    [ADD] add ugly account management

--- src/Front/AccountManagement/AccountManagement.tsx
+++ src/Front/AccountManagement/AccountManagement.tsx
@@ -1,5 +1,33 @@
+import {Form} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import React from "react";
+
 export default function accountManagement() {
     return (
-        <div></div>
+        <Form>
+            <Form.Group controlId="formUsernameLogin">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="username" />
+            </Form.Group>
+
+            <Form.Group controlId="formEmailLogin">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="email" placeholder="email" />
+            </Form.Group>
+
+            <Form.Group controlId="formPasswordLogin">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group controlId="formPasswordLoginAgain">
+                <Form.Label>Password (again)</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 }
